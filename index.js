8addf6172ecb001d57d3921efb2234453338ab7d
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-06 19:16:47 +0300

    Environment Variables

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) MIDDLEWARES
-app.use(morgan("dev"));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config.env",
+});
+
 const app = require("./index.js");
 
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
