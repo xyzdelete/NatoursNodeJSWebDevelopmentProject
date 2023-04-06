6db79f03d79ca8329e030a2f7a219860cc2ae921
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-06 15:27:08 +0300

    Responding to URL Parameters

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 // app.get("/", (req, res) => {
 //   res
 //     .status(200)
@@ -25,6 +27,48 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
