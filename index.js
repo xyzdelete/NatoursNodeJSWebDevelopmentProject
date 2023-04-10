03532216ebd07ab1907d7944abce54a73f9ae703
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-10 11:53:02 +0300

    Connecting Our Database with the Express App

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 // 1) MIDDLEWARES
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware 😊");
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./index");
 
 dotenv.config({
   path: "./config.env",
 });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB connection successful!");
+  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
