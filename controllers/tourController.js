5f4d5f78fb8a4be368eb1f05c9a68b4f8a97f666
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-11 11:29:14 +0300

    Making the API Better: Advanced Filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,21 +2,23 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
     // BUILD QUERY
+    // 1) FILTERING
     // eslint-disable-next-line node/no-unsupported-features/es-syntax
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    console.log(req.query, queryObj);
+    // 2) Advanced filtering
 
-    const query = await Tour.find(queryObj);
+    // { difficulty: "easy", duration: { $gte: 5 } }
+    // { difficulty: 'easy', duration: { gte: '5' } }
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    // const tours = await Tour.find()
-    //   .where("duration")
-    //   .equals(5)
-    //   .where("difficulty")
-    //   .equals("easy");
+    const query = await Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
     const tours = await query;
