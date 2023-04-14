b38cd57fa3354de3e7a53cb1c21fc763accbf6fe
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-14 15:41:49 +0300

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,15 @@
-const AppError = require("./../utils/appError");
+const AppError = require("../utils/appError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
 
@@ -47,6 +55,7 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
 
     if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.name === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
