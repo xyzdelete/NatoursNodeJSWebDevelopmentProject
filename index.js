12e9f3eb8ead58c443e7b3900aad57b970075a7a
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-14 13:18:11 +0300

    Handling Unhandled Routes

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware 👋");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -23,5 +18,13 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
 
 module.exports = app;
