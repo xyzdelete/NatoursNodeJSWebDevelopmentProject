a151c659453041c10721b1c290ba3325a3d0328d
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-16 16:48:53 +0300

    Data Sanitization

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -25,6 +27,13 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitazation again NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanatization agains XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
