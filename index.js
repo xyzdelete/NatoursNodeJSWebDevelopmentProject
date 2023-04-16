8f12ca520a279129ddd4431ee5b19395d816a277
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-16 16:35:40 +0300

    Setting Security HTTP Headers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,6 +9,8 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
+
+app.use(helmet());
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
@@ -21,7 +24,7 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
