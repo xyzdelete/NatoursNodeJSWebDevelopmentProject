e2569c1215cf7919d6adce0ffda4f914a84c9a61
Arturs Anikins <xyzdelete@protonmail.com>
2023-04-19 17:16:22 +0300

    Setting up the Project Structure

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
 const app = express();
 
@@ -69,12 +70,6 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The Forest Hiker",
-    user: "Arturs",
-  });
-});
 
 app.get("/overview", (req, res) => {
   res.status(200).render("overview", {
@@ -88,6 +83,7 @@ app.get("/tour", (req, res) => {
   });
 });
 
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
